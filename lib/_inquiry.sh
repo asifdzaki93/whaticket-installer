535c30e36cfced6e1620b059fccb2ac68505b7d1
asifdzaki93 <81837937+asifdzaki93@users.noreply.github.com>
2023-07-26 17:02:37 +0700

    bahasa indonesia

--- lib/_inquiry.sh
+++ lib/_inquiry.sh
@@ -1,40 +1,65 @@
 #!/bin/bash
 
+#######################################
+# Fungsi: print_banner
+# Argumen: None
+#######################################
+print_banner() {
+  # Fungsi ini bisa diisi dengan tampilan banner atau teks lainnya
+  # Sesuaikan dengan preferensi Anda
+  echo "======================================="
+}
+
+#######################################
+# Fungsi: get_frontend_url
+# Argumen: None
+#######################################
 get_frontend_url() {
-  
   print_banner
-  printf "${WHITE} 💻 Digite o domínio da interface web:${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Masukkan URL frontend:${GRAY_LIGHT}"
   printf "\n\n"
   read -p "> " frontend_url
 }
 
+#######################################
+# Fungsi: get_backend_url
+# Argumen: None
+#######################################
 get_backend_url() {
-  
   print_banner
-  printf "${WHITE} 💻 Digite o domínio da sua API:${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Masukkan URL backend API:${GRAY_LIGHT}"
   printf "\n\n"
   read -p "> " backend_url
 }
 
+#######################################
+# Fungsi: get_urls
+# Argumen: None
+#######################################
 get_urls() {
-  
   get_frontend_url
   get_backend_url
 }
 
+#######################################
+# Fungsi: software_update
+# Argumen: None
+#######################################
 software_update() {
-  
   frontend_update
   backend_update
 }
 
+#######################################
+# Fungsi: inquiry_options
+# Argumen: None
+#######################################
 inquiry_options() {
-  
   print_banner
-  printf "${WHITE} 💻 O que você precisa fazer?${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Apa yang perlu Anda lakukan?${GRAY_LIGHT}"
   printf "\n\n"
-  printf "   [1] Instalar\n"
-  printf "   [2] Atualizar\n"
+  printf "   [1] Instalasi\n"
+  printf "   [2] Pembaruan\n"
   printf "\n"
   read -p "> " option
 
@@ -49,4 +74,12 @@ inquiry_options() {
     *) exit ;;
   esac
 }
+
+# Panggil fungsi untuk menanyakan URL frontend dan backend API
+inquiry_options
+
+# Setelah mendapatkan URL frontend dan backend API, Anda dapat melanjutkan dengan implementasi fungsi lainnya sesuai kebutuhan.
+# Misalnya, Anda dapat membuat fungsi frontend_update dan backend_update untuk melakukan pembaruan perangkat lunak pada masing-masing bagian.
+# Setelah semua implementasi fungsi selesai, Anda bisa menambahkan tindakan terakhir atau pesan keluaran yang sesuai.
+# Selamat mengimplementasikan skrip shell sesuai kebutuhan Anda!
 
